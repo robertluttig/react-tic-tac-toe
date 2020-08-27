6ed09a3856f7b1f0b174b2195cba6a2dcb87f4a8
Robert Luttig <robert.luttig@gmail.com>
2020-08-27 15:06:43 -0700

    Updates

---
+++ precache-manifest.d6cd1a125030a71768fb9a9ee9137648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96750c874b09022448706283b0ae5c1a",
+    "url": "/robertluttig/react-tic-tac-toe.git/index.html"
+  },
+  {
+    "revision": "b0ee21ff491fbc956fd1",
+    "url": "/robertluttig/react-tic-tac-toe.git/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "6267ef04c2306825f87b",
+    "url": "/robertluttig/react-tic-tac-toe.git/static/js/2.a658cff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robertluttig/react-tic-tac-toe.git/static/js/2.a658cff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ee21ff491fbc956fd1",
+    "url": "/robertluttig/react-tic-tac-toe.git/static/js/main.5509c845.chunk.js"
+  },
+  {
+    "revision": "8303eb1d68e6172220da",
+    "url": "/robertluttig/react-tic-tac-toe.git/static/js/runtime-main.bb4ec1ed.js"
+  }
+]);
